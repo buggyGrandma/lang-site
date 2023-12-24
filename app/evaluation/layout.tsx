64f13b244dfedfb9392fa13c3cdd37f78664ce21
buggyGrandma <alireza.kh3221@gmail.com>
2023-12-24 12:45:54 +0330

    layout handled

---
+++ app/evaluation/layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import CloseIcon from "./Close";
+
+const EvaluationLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <main dir="rtl" className="md:pt-[56px] bg-gray-500 h-[100vh]">
+        <div className="border-b h-[64px] md:h-[91px] md:px-[172px] flex items-center gap-[8px] md:gap-[12px] w-full bg-white md:rounded-t-[24px]">
+          <div className="p-[8px] rounded-full md:bg-[#F2F2F2]">
+            <CloseIcon />
+          </div>
+          <div>آزمون تعیین سطح</div>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-[#F8F8FB] h-[100vh]">
+          {children}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default EvaluationLayout;
---
+++ app/evaluation/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PictuerMD from "./PictuerMD";
+import PictureSM from "./PictureSM";
+
+const Evaluation = () => {
+  return (
+    <>
+      <PictuerMD />
+      <PictureSM />
+    </>
+  );
+};
+
+export default Evaluation;
